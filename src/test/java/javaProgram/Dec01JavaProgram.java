0336a00eaca5c09babb474750610f6419813e76a
sharathgoud2016 <sharathgoud2016@gmail.com>
2023-12-03 21:47:42 +0530

    Updated

--- src/test/java/javaProgram/Dec01JavaProgram.java
+++ src/test/java/javaProgram/Dec01JavaProgram.java
@@ -4,7 +4,7 @@ public class Dec01JavaProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sharath");
+		System.out.println("Sharath02");
 
 	}
 
